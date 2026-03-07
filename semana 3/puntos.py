d3e61bcd7ca89063d476baed7ff9e3e5ce7af535
involency444 <tiagoredsiu@gmail.com>
2026-03-06 21:11:53 -0500

    Create puntos.py

---
+++ semana 3/puntos.py
@@ -0,0 +1,13 @@
+#ALGORITMO
+# 1 PEDIR EL PRIMER PUNTAJE
+# 2 PEDIR EL SEGUNDO PUNTAJE
+# COMPARAR PUNTAJE 1 CON PUNTAJE 2 
+#PSEUDOCODIGO
+# INICIO
+# LEER PUNTAJE1
+# LEER PUNTAJE2
+# ESCRIBIR SI PUNTAJE1 ES MAYOR QUE PUNTAJE2
+
+nacional = int(input("Ingrese el primer puntaje:"))
+millos = int(input("Ingrese el segundo puntaje:"))
+print(nacional>millos)
